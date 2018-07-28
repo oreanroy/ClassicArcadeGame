68cba1d6531cc9febfeaf6e604d9e1c83912eaa6
oreanroy <oreanroy@gmail.com>
2018-07-29 00:03:18 +0530

    I am lost

--- js/app.js
+++ js/app.js
@@ -1,12 +1,16 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
 };
+
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -14,6 +18,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.speed+=this.speed*dt;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,7 +29,32 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
+class player{
+    constructor(x, y){
+        this.sprite = 'images/char-cat-girl.png';
+        this.x = 5;
+        this.y = 0;
+        
+    }
+    update(){
+        this.render();
+    }
+    render(){
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+        //ctx.drawImage(Resources.get(this.guy), this.x, this.y);
+    }
+    handleInput(pos){
+        if(pos == 37 && x>=83) 
+            this.x-=83;
+            //101
+        if(pos == 38 && y<600)
+            this.y+=101;
+        if(pos == 39 && x<500) 
+            this.x+=83;
+        if(pos == 40 && y>=101)
+            this.y-=101;
+    }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
@@ -44,3 +74,9 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+player = new player();
+let allEnemies = []
+for(i=0;i<4;i++){
+    enemy = new Enemy(40, 50, 100);
+    allEnemies[i]=enemy
+}
